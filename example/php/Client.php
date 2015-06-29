34a5429be50bc5b298a0028ef912ad21f04780a8
Jesse <jesse@houzz.com>
2015-06-29 03:50:33 -0700

    fix path

--- example/php/Client.php
+++ example/php/Client.php
@@ -4,41 +4,42 @@ namespace tutorial\php;
 
 error_reporting(E_ALL);
 
-require_once __DIR__.'/../../lib/php/lib/Thrift/ClassLoader/ThriftClassLoader.php';
+require_once __DIR__.'/Thrift/ClassLoader/ThriftClassLoader.php';
 
 use Thrift\ClassLoader\ThriftClassLoader;
 
-$GEN_DIR = realpath(dirname(__FILE__).'/..').'/gen-php';
+$GEN_DIR = realpath(dirname(__FILE__).'/..').'/php';
 
 $loader = new ThriftClassLoader();
-$loader->registerNamespace('Thrift', __DIR__ . '/../../lib/php/lib');
+$loader->registerNamespace('Thrift', __DIR__);
 $loader->registerDefinition('shared', $GEN_DIR);
 $loader->registerDefinition('tutorial', $GEN_DIR);
 $loader->register();
 
 /*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements. See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership. The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License. You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+* Licensed to the Apache Software Foundation (ASF) under one
+* or more contributor license agreements. See the NOTICE file
+* distributed with this work for additional information
+* regarding copyright ownership. The ASF licenses this file
+* to you under the Apache License, Version 2.0 (the
+* "License"); you may not use this file except in compliance
+* with the License. You may obtain a copy of the License at
+*
+*   http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing,
+* software distributed under the License is distributed on an
+* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+* KIND, either express or implied. See the License for the
+* specific language governing permissions and limitations
+* under the License.
+*/
 
 use Thrift\Protocol\TBinaryProtocol;
 use Thrift\Transport\TSocket;
 use Thrift\Transport\THttpClient;
 use Thrift\Transport\TBufferedTransport;
+use Thrift\Transport\TFramedTransport;
 use Thrift\Exception\TException;
 
 try {
@@ -47,7 +48,7 @@ try {
     } else {
         $socket = new TSocket('localhost', 9090);
     }
-    $transport = new TBufferedTransport($socket, 1024, 1024);
+    $transport = new TFramedTransport($socket, 1024, 1024);
     $protocol = new TBinaryProtocol($transport);
     $client = new \tutorial\CalculatorClient($protocol);
 
